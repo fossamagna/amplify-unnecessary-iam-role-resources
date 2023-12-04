18ad0cfcfd899a4fdcd2b35362c6d7b33eed553f
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2023-12-04 22:53:12 +0900

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyiamrole": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  }
+}
